0f8c8f5128b541a44d997e4bd8100ceb252dae20
gregwoodio <contact@gregwoodio>
2020-05-12 20:29:41 -0400

    2.4 - Parser's first steps: parsing let statements

---
+++ src/monkey/ast/ast.go
@@ -0,0 +1,60 @@
+package ast
+
+import (
+	"monkey/token"
+)
+
+type Node interface {
+	TokenLiteral() string
+}
+
+type Statement interface {
+	Node
+	statementNode()
+}
+
+type Expression interface {
+	Node
+	expressionNode()
+}
+
+// Program is the root of our AST, and it contains a slice of statements
+type Program struct {
+	Statements []Statement
+}
+
+// TokenLiteral for Program node
+func (p *Program) TokenLiteral() string {
+	if len(p.Statements) > 0 {
+		return p.Statements[0].TokenLiteral()
+	} else {
+		return ""
+	}
+}
+
+// LetStatement is an AST node representing a let statement
+type LetStatement struct {
+	Token token.Token // the token.LET token
+	Name  *Identifier
+	Value Expression
+}
+
+func (ls *LetStatement) statementNode() {}
+
+// TokenLiteral for LetStatement token
+func (ls *LetStatement) TokenLiteral() string {
+	return ls.Token.Literal
+}
+
+// Identifier is an AST node representing an identifier
+type Identifier struct {
+	Token token.Token // the token.IDENT token
+	Value string
+}
+
+func (i *Identifier) expressionNode() {}
+
+// TokenLiteral for Identifier token
+func (i *Identifier) TokenLiteral() string {
+	return i.Token.Literal
+}

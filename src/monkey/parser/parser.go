b74adf5df0fcf636ab651197e3eef3d1e41bda17
gregwoodio <contact@gregwoodio>
2020-05-16 17:54:05 -0400

    2.5 - Parsing return statements

--- src/monkey/parser/parser.go
+++ src/monkey/parser/parser.go
@@ -63,13 +63,17 @@ func (p *Parser) parseStatement() ast.Statement {
 	switch p.curToken.Type {
 	case token.LET:
 		return p.parseLetStatement()
+	case token.RETURN:
+		return p.parseReturnStatement()
 	default:
 		return nil
 	}
 }
 
 func (p *Parser) parseLetStatement() *ast.LetStatement {
-	stmt := &ast.LetStatement{Token: p.curToken}
+	stmt := &ast.LetStatement{
+		Token: p.curToken,
+	}
 
 	if !p.expectPeek(token.IDENT) {
 		return nil
@@ -85,6 +89,22 @@ func (p *Parser) parseLetStatement() *ast.LetStatement {
 	}
 
 	// TODO: We're skipping the expressions until we encounter a semicolon
+	for !p.curTokenIs(token.SEMICOLON) {
+		p.nextToken()
+	}
+
+	return stmt
+}
+
+func (p *Parser) parseReturnStatement() *ast.ReturnStatement {
+	stmt := &ast.ReturnStatement{
+		Token: p.curToken,
+	}
+
+	p.nextToken()
+
+	// TODO: We're skipping the expressions until
+	// we encounter a semicolon
 	for !p.curTokenIs(token.SEMICOLON) {
 		p.nextToken()
 	}

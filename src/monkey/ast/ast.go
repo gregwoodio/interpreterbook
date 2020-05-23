40dff8defc052948fb23e9f512494a2fe0ae9af4
Greg Wood <contact@gregwood.io>
2020-05-23 15:22:42 -0400

    2.6 - Parsing Expressions, integer literals

--- src/monkey/ast/ast.go
+++ src/monkey/ast/ast.go
@@ -146,3 +146,20 @@ func (es *ExpressionStatement) String() string {
 
 	return ""
 }
+
+// IntegerLiteral is an expression representing an integer
+type IntegerLiteral struct {
+	Token token.Token
+	Value int64
+}
+
+func (il *IntegerLiteral) expressionNode() {}
+
+// TokenLiteral for IntegerLiteral
+func (il *IntegerLiteral) TokenLiteral() string {
+	return il.Token.Literal
+}
+
+func (il *IntegerLiteral) String() string {
+	return il.Token.Literal
+}

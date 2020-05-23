097922428ce13e362281c7d8f1f9a16134e3c546
Greg Wood <contact@gregwood.io>
2020-05-23 15:45:34 -0400

    2.6 - Parsing Expressions, prefix expressions

--- src/monkey/ast/ast.go
+++ src/monkey/ast/ast.go
@@ -163,3 +163,27 @@ func (il *IntegerLiteral) TokenLiteral() string {
 func (il *IntegerLiteral) String() string {
 	return il.Token.Literal
 }
+
+// PrefixExpression is an expression before another expression, such as -1 or !true
+type PrefixExpression struct {
+	Token    token.Token //the prefix token, eg !
+	Operator string
+	Right    Expression
+}
+
+func (pe *PrefixExpression) expressionNode() {}
+
+// TokenLiteral for PrefixExpression
+func (pe *PrefixExpression) TokenLiteral() string {
+	return pe.Token.Literal
+}
+
+func (pe *PrefixExpression) String() string {
+	var out bytes.Buffer
+	out.WriteString("(")
+	out.WriteString(pe.Operator)
+	out.WriteString(pe.Right.String())
+	out.WriteString(")")
+
+	return out.String()
+}

7d283028d737e86397052b0a43a29ee1deab7f50
gregwoodio <contact@gregwoodio>
2020-06-07 16:42:12 -0400

    3.6 Prefix expressions

--- src/monkey/evaluator/evaluator.go
+++ src/monkey/evaluator/evaluator.go
@@ -6,10 +6,13 @@ import (
 )
 
 var (
-	// TRUE is a constant object.Boolean used for true
+	// NULL is a constant object.Null used for all null values
+	NULL = &object.Null{}
+
+	// TRUE is a constant object.Boolean used for all true values
 	TRUE = &object.Boolean{Value: true}
 
-	// FALSE is a constrant object.Boolean used for false
+	// FALSE is a constrant object.Boolean used for all false values
 	FALSE = &object.Boolean{Value: false}
 )
 
@@ -30,6 +33,10 @@ func Eval(node ast.Node) object.Object {
 
 	case *ast.Boolean:
 		return nativeBoolToBooleanObject(node.Value)
+
+	case *ast.PrefixExpression:
+		right := Eval(node.Right)
+		return evalPrefixExpression(node.Operator, right)
 	}
 
 	return nil
@@ -43,6 +50,39 @@ func evalStatements(stmts []ast.Statement) object.Object {
 	}
 
 	return result
+}
+
+func evalPrefixExpression(operator string, right object.Object) object.Object {
+	switch operator {
+	case "!":
+		return evalBangOperatorExpression(right)
+	case "-":
+		return evalMinusPrefixOperatorExpression(right)
+	default:
+		return NULL
+	}
+}
+
+func evalBangOperatorExpression(right object.Object) object.Object {
+	switch right {
+	case TRUE:
+		return FALSE
+	case FALSE:
+		return TRUE
+	case NULL:
+		return TRUE
+	default:
+		return FALSE
+	}
+}
+
+func evalMinusPrefixOperatorExpression(right object.Object) object.Object {
+	if right.Type() != object.INTEGER_OBJ {
+		return NULL
+	}
+
+	value := right.(*object.Integer).Value
+	return &object.Integer{Value: -value}
 }
 
 func nativeBoolToBooleanObject(input bool) *object.Boolean {

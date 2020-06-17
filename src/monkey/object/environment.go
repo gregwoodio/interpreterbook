a364f09361252e49f16b4cf83ccdc8789b084063
Greg Wood <contact@gregwood.io>
2020-06-16 22:18:37 -0400

    3.10 - Functions & function calls

--- src/monkey/object/environment.go
+++ src/monkey/object/environment.go
@@ -1,21 +1,32 @@
 package object
 
+func NewEnclosedEnvironment(outer *Environment) *Environment {
+	env := NewEnvironment()
+	env.outer = outer
+	return env
+}
+
 // NewEnvironment creates a new environment
 func NewEnvironment() *Environment {
 	s := make(map[string]Object)
 	return &Environment{
 		store: s,
+		outer: nil,
 	}
 }
 
 // Environment type keeps track of our variables
 type Environment struct {
 	store map[string]Object
+	outer *Environment
 }
 
 // Get a variable's value
 func (e *Environment) Get(name string) (Object, bool) {
 	obj, ok := e.store[name]
+	if !ok && e.outer != nil {
+		obj, ok = e.outer.Get(name)
+	}
 	return obj, ok
 }
 
